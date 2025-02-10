d9f718dae3b8192e874ea70a402a82db34dfeb72
creation <creation2224@gmail.com>
2025-02-10 12:46:22 +0530

    reduxtoolkitTODO

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+
+  return (
+    <>
+    <h1>
+      Todos using RTK!
+    </h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
